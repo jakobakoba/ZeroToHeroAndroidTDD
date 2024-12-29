784ca6f436703988a51fe68b5cee8049fea0958e
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-29 19:43:38 +0900

    good solution

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -9,47 +9,34 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
-        val button = binding.actionButton
-        val editText = binding.inputEditText
-        textView = binding.titleTextView
+        val textWatcher = object : SimpleTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                binding.actionButton.isEnabled = binding.inputEditText.text.toString().length >= 3
 
-        button.isEnabled = false
 
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.toString().length >= 3) button.isEnabled = true else button.isEnabled = false
             }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
-
-        button.setOnClickListener {
-            textView.text = editText.text
-            editText.setText("")
         }
 
+        binding.actionButton.setOnClickListener {
+            binding.titleTextView.text = binding.inputEditText.text
+            binding.inputEditText.setText("")
+        }
+
+        binding.inputEditText.addTextChangedListener(textWatcher)
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(KEY, textView.text.toString())
-    }
+}
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        textView.text = savedInstanceState.getString(KEY)
-    }
+private abstract class SimpleTextWatcher : TextWatcher {
+    override fun afterTextChanged(s: Editable?) = Unit
 
-    companion object {
-        private const val KEY = "KEY"
-    }
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
 }
