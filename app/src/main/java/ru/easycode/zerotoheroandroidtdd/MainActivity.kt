4cac1c944268edeb22bb516a38cb2aa20c52bb47
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-30 12:59:09 +0900

    good solution with list_item and ScrollView

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,11 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+import ru.easycode.zerotoheroandroidtdd.databinding.ListItemBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -40,9 +40,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addTextView(inputText: String) {
-        val textView = TextView(this).apply {
-            text = inputText
-        }
+        val listBinding = ListItemBinding.inflate(layoutInflater)
+        val textView = listBinding.root
+        textView.text = inputText
         binding.contentLayout.addView(textView)
     }
 
