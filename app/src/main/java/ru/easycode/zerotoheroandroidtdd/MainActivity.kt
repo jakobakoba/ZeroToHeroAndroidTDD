6658a91fe555428a82355a10c2e534a2bb9a10d1
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-30 12:38:51 +0900

    good solution

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,47 +1,49 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.app.ActionBar.LayoutParams
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val textViews = mutableListOf<String>()
-    private lateinit var layout : LinearLayout
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
         val button = binding.actionButton
         val editText = binding.inputEditText
-        layout = binding.contentLayout
 
         button.setOnClickListener{
-            val textView = TextView(this)
-            textView.text = editText.text
+            addTextView(editText.text.toString())
             editText.setText("")
-            layout.addView(textView)
-            textViews.add(textView.text.toString())
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putStringArray(KEY, textViews.toTypedArray())
+        val textList = binding.contentLayout.children.map { (it as TextView).text.toString() }.toList()
+        outState.putStringArrayList(KEY, ArrayList(textList))
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        savedInstanceState.getStringArray(KEY)?.let { textViews.addAll(it) }
-        for (i in 0 until textViews.size){
-            val textView = TextView(this)
-            textView.text = textViews[i]
-            layout.addView(textView)
+        val list = savedInstanceState.getStringArrayList(KEY) ?: ArrayList()
+        list.forEach {inputText ->
+            addTextView(inputText)
         }
+
+    }
+
+    private fun addTextView(inputText: String) {
+        val textView = TextView(this).apply {
+            text = inputText
+        }
+        binding.contentLayout.addView(textView)
     }
 
     companion object {
