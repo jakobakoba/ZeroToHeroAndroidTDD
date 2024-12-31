1ab038c5b526b23b6fc2d9ce16fc26f4cdeb8e55
jakobakoba <borisgabyshev96dev@gmail.com>
2024-12-31 20:12:17 +0900

    have to look up previous tasks

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/RecyclerViewAdapter.kt
@@ -0,0 +1,25 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter: RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}
+
+class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+     fun bind(text: String) {
+         itemView.
+     }
+}
