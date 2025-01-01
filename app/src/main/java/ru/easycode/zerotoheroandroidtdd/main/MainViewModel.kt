7ebb015fd27c53f3f5e419000ac867fefc1ba90a
jakobakoba <borisgabyshev96dev@gmail.com>
2025-01-01 18:47:26 +0900

    have to get BundleWrapper and LiveDataWrapper

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/main/MainViewModel.kt
@@ -0,0 +1,12 @@
+package ru.easycode.zerotoheroandroidtdd.main
+
+class MainViewModel(
+    private val navigation: Navigation.Mutable
+) {
+
+    fun init(firstRun: Boolean) {
+
+    }
+
+
+}
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/main/Navigation.kt
@@ -0,0 +1,6 @@
+package ru.easycode.zerotoheroandroidtdd.main
+
+interface Navigation {
+
+    interface Mutable
+}
